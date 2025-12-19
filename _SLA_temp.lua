16c687a69b771db573a9bf2b18f9373a1ebda15b
heibaizhiyi <80760174+heibaizhiyi@users.noreply.github.com>
2025-12-19 17:54:04 +0800

    Add files via upload

---
+++ _SLA_temp.lua
@@ -0,0 +1,2 @@
+scripts_lastModified = 1765246553482
+_SL_version_info = '09日10时15分53秒 (2)'
---
+++ lua/env_vars.lua
@@ -0,0 +1,9 @@
+-- 本文件由环境生成代码
+--
+local envars = {
+    local_map_dir_path = "C:\\Users\\xxx\\solar\\solar_rpg_editor\\game_root\\",
+    listFile = "Ky+tSAb8"
+
+
+}
+return envars
